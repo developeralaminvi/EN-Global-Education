918e37ca9028243ec289813efe115e4cffeb5d98
developeralamin <developeralaminvi@gmail.com>
2024-11-06 11:12:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+  $(".partnersCarousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false,
+        margin: 20,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".team-content-1-slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 2,
+        nav: true,
+        loop: false,
+        margin: 20,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".operations-content-1-slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+        margin: 20,
+      },
+    },
+  });
+});
+
+
+// Preload
+
+$(document).ready(function () {
+  $(window).on("load", function () {
+    $(".loader-animation").fadeOut();
+  });
+});
+
+
+// Back to top
+var amountScrolled = 200;
+var amountScrolledNav = 25;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled ) {
+    $('button.back-to-top').addClass('show');
+  } else {
+    $('button.back-to-top').removeClass('show');
+  }
+});
+
+$('button.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 800);
+  return false;
+});
+
+
